6ac6116d865f51ccc6376473cd858e1ed96318ea
haegelix <haegelix@yahoo.de>
2021-11-26 15:48:54 +0100

    added comments and deleted unnecessary code

--- run.bash
+++ run.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
+
+# source paths for ros2 suite
 source /opt/ros/foxy/setup.bash
+
+# set environment to enable dev-tools e.g. detection of changed files
 export FLASK_ENV=development
-#flask run --host=0.0.0.0
+
 python3 app.py
