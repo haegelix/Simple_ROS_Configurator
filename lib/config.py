676ef1ae9a241110a5ce9832fe97af1ea937df79
haegelix <haegelix@yahoo.de>
2021-12-07 17:00:44 +0100

    Add command line parameters #8
    config now loads values from accepted command line parameters. these override the ones from the config files

--- lib/config.py
+++ lib/config.py
@@ -1,4 +1,9 @@
 import json
+import sys
+import getopt
+import re
+
+ending_in_json = re.compile(r'[\w -]*[.]json')
 
 
 class Config(object):
@@ -9,16 +14,64 @@ class Config(object):
     ros_source_path: str
     ignore_configs: [str]
     always_yes: bool
+    selected_pkg_config: str
 
     def __init__(self, path="config.json"):
-        js = json.load(open(path))
-        self.mode = js["mode"]
-        self.wspath = js["wspath"]
-        self.foreign_repos_config = js["foreign_repos_config"]
-        self.ignore_configs = js["ignore_configs"]
-        self.rosdistro = js["rosdistro"]
-        self.ros_source_path = js["ros_source_path"]
-        self.always_yes = js["always_yes"]
+        # load config from file
+        self.__parse_file__(path)
+        self.__parse_argv__()
+
+    def __parse_file__(self, path: str):
+        """
+        Parse the given config file.
+        :param path: String containing the path to the config file.
+        """
+        conf = json.load(open(path))
+        self.mode = conf["mode"]
+        self.wspath = conf["wspath"]
+        self.foreign_repos_config = conf["foreign_repos_config"]
+        self.ignore_configs = conf["ignore_configs"]
+        self.rosdistro = conf["rosdistro"]
+        self.ros_source_path = conf["ros_source_path"]
+        self.always_yes = conf["always_yes"]
+
+    def __parse_argv__(self):
+        """
+        Checks for command line parameters and overrides config values.
+        May exit the app.
+        """
+        try:
+            argv = sys.argv[1:]  # name of this app shall not be searched for flags
+            opts, args = getopt.getopt(argv, "hviys:", ["help", "version", "intercative", "yes", "select="])
+        except getopt.GetoptError:
+            print_help()
+            sys.exit(2)
+
+        o, a = zip(*opts)
+
+        # search for version or help flags
+        if "-h" in o or "--help" in o:
+            print_help()
+            sys.exit()
+        if "-v" in o or "--version" in o:
+            print_version()
+            sys.exit()
+
+        # search for all other valid flags and set config
+        for opt, arg in opts:
+            if opt in ("-i", "--interactive"):
+                self.always_yes = False
+                pass  # TODO more interactiveness?
+            elif opt in ("-y", "--yes"):
+                self.always_yes = True
+            elif opt in ("-s", "--select"):
+                sel = arg
+                if not ending_in_json.match(sel):
+                    sel = sel + ".json"
+                print("Selected config file:", sel)
+                self.selected_pkg_config = sel
+            else:
+                pass  # this branch is unreachable
 
     def __str__(self):
         s = ""
@@ -30,6 +83,22 @@ class Config(object):
         s += "ROS source-file path:         " + self.ros_source_path + "\n"
         s += "Always assume yes?            " + str(self.always_yes)
         return s
+
+
+def print_help():
+    print_version()
+    print("")
+    print("Usage:")
+    print(sys.argv[0], ' [-h] [-i | -y] [-s <config_name>]')
+    print("-h --help                Will produce this output and then exit.")
+    print("-v --version             Will print version details and then exit.")
+    print("-i --interactive         Force app to interact with you.")
+    print("-y --yes                 Force app to run without interaction. USE WITH CAUTION!")
+    print("-s <cfg> --select=<cfg>  Select specific package to be built from config. '.json' may be omitted.")
+
+
+def print_version():
+    print("Version (TODO)")  # TODO print version
 
 
 config = Config()
