906d7e3f4ce9fe917d668c92982234466aa58dda
haegelix <haegelix@yahoo.de>
2021-12-10 20:45:55 +0100

    setup.sh fixes

--- src/lib/setup.sh
+++ src/lib/setup.sh
@@ -4,20 +4,20 @@ LIB_DIR="/usr/local/lib/srosc/"
 CFG_DIR="/etc/srosc/"
 
 # srosc
-chmod +x ${BIN_DIR}srosc
-chmod +x ${LIB_DIR}srosc.py
+chmod o+x ${BIN_DIR}srosc
+chmod o+x ${LIB_DIR}srosc.py
 
 # runconfigs
-chmod +x ${LIB_DIR}runconfigs.sh
-chmod +x ${LIB_DIR}runconfigs.py
+chmod o+x ${LIB_DIR}runconfigs.sh
+chmod o+x ${LIB_DIR}runconfigs.py
 
 # text ui
-chmod +x ${LIB_DIR}newconfig.py
+chmod o+x ${LIB_DIR}newconfig.py
 
 # web ui
-chmod +x ${LIB_DIR}runserver.sh
-chmod +x ${LIB_DIR}runserver.py
-chmod +x ${LIB_DIR}app.py
+chmod o+x ${LIB_DIR}runserver.sh
+chmod o+x ${LIB_DIR}runserver.py
+chmod o+x ${LIB_DIR}app.py
 
 # config
 chmod 644 ${CFG_DIR}config.json
