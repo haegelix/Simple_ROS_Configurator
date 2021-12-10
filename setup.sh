db23ac895f7be511bfdceb9e244f0dc136bbb898
haegelix <haegelix@yahoo.de>
2021-12-10 14:21:42 +0100

    setup now cares about more files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# setup
+chmod +x setup.sh
+
+# srosc
+chmod +x srosc
+chmod +x srosc.py
+
 # runconfigs
 chmod +x runconfigs.sh
 chmod +x runconfigs.py
@@ -8,4 +15,5 @@ chmod +x newconfig.py
 
 # web ui
 chmod +x runserver.sh
+chmod +x runserver.py
 chmod +x app.py
