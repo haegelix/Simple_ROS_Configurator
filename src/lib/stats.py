e89d15621c56043298d4a6c3aa8c4f1543e4262d
haegelix <haegelix@yahoo.de>
2021-12-15 10:42:01 +0100

    stats initial dummyfile commit

---
+++ src/lib/stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+import os
+
+# TODO implement stats
+"""
+TODO:
+- Server started? + Details if yes
+- Path of config file
+- version
+- 
+"""
+
+
+def main(argv):
+    print(os.getcwd())
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
