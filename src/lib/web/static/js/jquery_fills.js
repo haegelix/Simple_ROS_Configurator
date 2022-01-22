0e199aa644f6281244c8146c64fa042b3fded453
haegelix <haegelix@yahoo.de>
2022-01-22 12:27:26 +0100

    filling holes in jquerys api

---
+++ src/lib/web/static/js/jquery_fills.js
@@ -0,0 +1,26 @@
+$.fn.extend({
+    /*showModal: function () {
+        return this.each(function () {
+            if (this.tagName === "DIALOG") {
+                console.log(this.id)
+                document.getElementById(this.id).showModal()
+                //this.setAttribute("open","");
+            }
+        });
+    },
+    close: function () {
+        return this.each(function () {
+            if (this.tagName === "DIALOG") {
+                console.log(this.id)
+                document.getElementById(this.id).close()
+            }
+        });
+    },
+    reset: function () {
+        return this.each(function () {
+            if (this.tagName === "FORM") {
+                this.reset();
+            }
+        });
+    },*/
+});
